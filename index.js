d2af8cdfcb54fe91d533d51fe620a62fc4c4de1d
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-08 21:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,10 +20,6 @@ function currentLine(line) {
     return "The line is currently empty.";
   }
   else {
-    var line = "The line is currently: ";
-    for (let i = 0; i < line.length; i++) {
-      line = line + `${i + 1}. ${line[i]}`;
-    }
-    return line;
+    return "The line is currently: " + line;
   }
 }
